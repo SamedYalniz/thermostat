da70dea7ae6dc80d8d686f523f1edf91c8dca618
Samed Yalniz <s.yalniz@hotmail.de>
2016-08-03 12:42:04 +0200

    thermostat with turning power saving mode on and off

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -19,5 +19,37 @@ describe('Thermostat', function() {
     thermostat.tempDown();
     expect(thermostat.temp).toEqual(19);
   })
+  it ("has a minimum temperature of 10 degrees", function() {
+    for (var i=0; i <=10; i++) {
+      thermostat.tempDown();
+    }
+    expect(thermostat.temp).toEqual(10);
+
+  })
+  describe ("power saving mode is on", function () {
+    it ("has a maximum temperature of 25 degrees", function () {
+      thermostat.switchPSMOn();
+      expect(thermostat.MAX_TEMP).toEqual(25);
+    })
+    it ("cannot increase higher than the max temp",function() {
+      for (var i=0; i <= 5; i++) {
+        thermostat.tempUp();
+      }
+      expect(thermostat.temp).toEqual(25);
+    })
+  })
+  describe ("power saving mode is off", function() {
+    it("has a maxmum temperature of 32 degrees", function() {
+      thermostat.switchPSMOff();
+      expect(thermostat.MAX_TEMP).toEqual(32);
+    })
+    it("cannot increase higher than the max temp",function(){
+      thermostat.switchPSMOff();
+      for (var i=0; i<= 12; i++) {
+        thermostat.tempUp();
+      }
+      expect(thermostat.temp).toEqual(32);
+    })
+  })
 
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,28 @@
 function Thermostat() {
   this.temp = 20;
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP = 25;
+
+
 };
 
 Thermostat.prototype = {
   tempUp: function() {
+    if (this.temp < this.MAX_TEMP)
     this.temp += 1;
   },
   tempDown: function() {
-    this.temp--;
+    if (this.temp > this.MIN_TEMP ) {
+      this.temp--;
+    }
   },
+
+  switchPSMOn: function() {
+    this.MAX_TEMP = 25;
+  },
+
+  switchPSMOff: function() {
+    this.MAX_TEMP = 32;
+  },
+
 }
