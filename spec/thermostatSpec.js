978d80b9203505ff5cd287d1c1d3bb8bca6ab5f8
benjamin-white <benjaminwhite@live.co.uk>
2016-08-03 14:13:20 +0100

    refactor magic numbers

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Thermostat', function() {
 
   var thermostat;
@@ -6,26 +8,45 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
+  it('has power saving enabled by default', function() {
+     expect(thermostat.isPowerSavingModeOn()).toEqual(true);
+  })
+
   it('starts at twenty degrees', function() {
-    expect(thermostat.temp).toEqual(20);
+    expect(thermostat.getCurrentTemp()).toEqual(20);
   });
 
   it('increases temperature when Up is called', function() {
     thermostat.tempUp();
-    expect(thermostat.temp).toEqual(21);
+    expect(thermostat.getCurrentTemp()).toEqual(21);
   })
 
   it('deecreases temperature when Down is called', function() {
     thermostat.tempDown();
-    expect(thermostat.temp).toEqual(19);
+    expect(thermostat.getCurrentTemp()).toEqual(19);
   })
   it ("has a minimum temperature of 10 degrees", function() {
     for (var i=0; i <=10; i++) {
       thermostat.tempDown();
     }
-    expect(thermostat.temp).toEqual(10);
+    expect(thermostat.getCurrentTemp()).toEqual(10);
 
   })
+
+  it('has a buttom to reset the temperature', function() {
+    thermostat.tempUp();
+    thermostat.resetTemp();
+    expect(thermostat.getCurrentTemp()).toEqual(20);
+  })
+
+    it('the interface is colored by temperature', function() {
+      for (var i = 0; i < 4; i++) {
+        thermostat.tempDown();
+      }
+      thermostat.color();
+      expect(thermostat.color).toEqual('green');
+    })
+
   describe ("power saving mode is on", function () {
     it ("has a maximum temperature of 25 degrees", function () {
       thermostat.switchPSMOn();
@@ -35,7 +56,7 @@ describe('Thermostat', function() {
       for (var i=0; i <= 5; i++) {
         thermostat.tempUp();
       }
-      expect(thermostat.temp).toEqual(25);
+      expect(thermostat.getCurrentTemp()).toEqual(25);
     })
   })
   describe ("power saving mode is off", function() {
@@ -48,7 +69,7 @@ describe('Thermostat', function() {
       for (var i=0; i<= 12; i++) {
         thermostat.tempUp();
       }
-      expect(thermostat.temp).toEqual(32);
+      expect(thermostat.getCurrentTemp()).toEqual(32);
     })
   })
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,28 +1,68 @@
+'use strict';
+
 function Thermostat() {
-  this.temp = 20;
+  this.DEFAULT_TEMPERATURE = 20;
+  this._temp = this.DEFAULT_TEMPERATURE;
   this.MIN_TEMP = 10;
   this.MAX_TEMP = 25;
-
-
+  this.PSM = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+  this.HIGH_ENERGY_USAGE_LIMIT = 25;
+  this.color;
 };
 
 Thermostat.prototype = {
+  getCurrentTemp: function() {
+    return this._temp;
+  },
   tempUp: function() {
-    if (this.temp < this.MAX_TEMP)
-    this.temp += 1;
+    if (!this.isMaxTemp())
+      this._temp += 1;
+  },
+  isMaxTemp: function() {
+    return this.getCurrentTemp() === this.MAX_TEMP;
   },
   tempDown: function() {
-    if (this.temp > this.MIN_TEMP ) {
-      this.temp--;
+    if (!this.isMinTemp())
+      this._temp--;
+  },
+  isMinTemp: function() {
+    return this.getCurrentTemp() === this.MIN_TEMP;
+  },
+  switchPSMOn: function() {
+    this.PSM = true;
+    this.maxTemp();
+  },
+  isPowerSavingModeOn: function() {
+    return this.PSM;
+  },
+  switchPSMOff: function() {
+    this.PSM = false;
+    this.maxTemp();
+  },
+
+  maxTemp: function() {
+    if (this.PSM) {
+      this.MAX_TEMP = this.MAX_LIMIT_PSM_ON;
+    } else {
+      this.MAX_TEMP = this.MAX_LIMIT_PSM_OFF;
     }
   },
 
-  switchPSMOn: function() {
-    this.MAX_TEMP = 25;
+  resetTemp: function() {
+    this._temp = this.DEFAULT_TEMPERATURE;
   },
 
-  switchPSMOff: function() {
-    this.MAX_TEMP = 32;
+  color: function() {
+    if (this.getCurrentTemp() < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      this.color = 'green';
+    } else if (this.getCurrentTemp() <= this.HIGH_ENERGY_USAGE_LIMIT) {
+        this.color = 'yellow';
+      } else {
+        this.color = 'red';
+      }
   },
 
 }
